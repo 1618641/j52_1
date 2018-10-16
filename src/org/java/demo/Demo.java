691a8b82995c0d858ac929f246560cc763baec7f
maguo <1618641@qq.com>
2018-10-17 00:11:57 +0800

    1111111

---
+++ src/org/java/demo/Demo.java
@@ -0,0 +1,13 @@
+package org.java.demo;
+
+/**
+ * @Author: 马果
+ * @Date: 2018/10/17 00:08
+ * @Description:
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("你好，世界");
+    }
+}
